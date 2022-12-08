ee07fd9fefc2105fc8a43dd2ca16e4009d0af6ac
hussein <asd>
2022-12-08 14:38:08 +0200

    New Commit

--- models/DB_URL.js
+++ models/DB_URL.js
@@ -1,3 +1,3 @@
-const DB_URL = "mongodb://localhost:27017/out-circle";
+const DB_URL = "mongodb+srv://anas:a1n2a3s4@out-circle.f6rq7ac.mongodb.net/?retryWrites=true&w=majority";
 
 module.exports = DB_URL;
